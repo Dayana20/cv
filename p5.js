7dc585ec83398134ccd13631a786c9c753abed29
Dayana20 <39806222+Dayana20@users.noreply.github.com>
2021-12-26 19:22:54 -0500

    updated main page

---
+++ p5.js
@@ -0,0 +1,35 @@
+
+
+function setup() {
+  var myDiv = document.getElementById("game");
+  var myWidth = myDiv.offsetWidth;
+  var myHeight = myDiv.offsetHeight;
+  var myCanvas = createCanvas(myWidth, myHeight);
+//   var myCanvas = createCanvas(800, 800);
+  myCanvas.parent("game");
+}
+
+function draw() {
+    
+  background(239,247,246);
+  noStroke(); // no border
+  // head
+  fill(216,191,216);
+  ellipse(mouseX+5, 150, 200,250);
+  // arms left
+  ellipse(mouseX-18, 300, 40,200);
+  ellipse(mouseX-45, 250, 50,200);
+  ellipse(mouseX-80, 200, 50,200);
+  
+  // arms right
+  ellipse(mouseX+18, 300, 40,200);
+  ellipse(mouseX+45, 250, 50,200);
+  ellipse(mouseX+80, 200, 50,200);
+
+  //eyes
+  fill(0);
+  ellipse(mouseX-18, 300, 13,8);
+  ellipse(mouseX+18, 300, 13,8);
+  
+}
+
