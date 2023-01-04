c83870a3d3e8edc5b095e08853a17d271e283b67
Dayana Alejandro <dayana200120@gmail.com>
2023-01-04 01:51:22 -0500

    making imgs resizable

---
+++ transitions.js
@@ -0,0 +1,29 @@
+function designPic1(){ 
+    if (document.getElementById("image1").style.display == "block"){
+        document.getElementById("image1")
+                    .style.display = "none";
+    } else{
+        document.getElementById("image1")
+                        .style.display = "block";
+    }
+}
+
+function designPic2(){ 
+    if (document.getElementById("image2").style.display == "block"){
+        document.getElementById("image2")
+                    .style.display = "none";
+    } else{
+        document.getElementById("image2")
+                        .style.display = "block";
+    }
+}
+
+function designPic3(){ 
+    if (document.getElementById("image3").style.display == "block"){
+        document.getElementById("image3")
+                    .style.display = "none";
+    } else{
+        document.getElementById("image3")
+                        .style.display = "block";
+    }
+}
